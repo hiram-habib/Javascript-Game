5fcf4e26d1dd8d0fcb8acf542cca61d4391aba79
hiram-habib <hiramh345@gmail.com>
2021-08-04 17:12:18 +1200

    Created projectiles (still stationary)

--- index.js
+++ index.js
@@ -21,9 +21,30 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, colour, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.colour = colour
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.colour
+        c.fill()
+    }
+}
 const x = canvas.width/2
 const y = canvas.height/2
 
 const player = new Player(x, y, 30, 'blue')
 
 player.draw()
+
+addEventListener('click', (event) => 
+    {const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
+        projectile.draw()}
+    )
